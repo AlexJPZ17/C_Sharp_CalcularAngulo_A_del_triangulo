d2a2e829f86042f48b6893dbb9acf7b7e0051a53
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-01 14:08:52 -0600

    C Sharp Calcular Angulo A

---
+++ CalcularAngulo A del triangulo/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CalcularAngulo_A_del_triangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label_AnguloB = new System.Windows.Forms.Label();
+            this.Label_AnguloA = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.TextBox_Adyacente = new System.Windows.Forms.TextBox();
+            this.TextBox_Hipotenusa = new System.Windows.Forms.TextBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button_Calular = new System.Windows.Forms.Button();
+            this.Button_Limpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label_AnguloB
+            // 
+            this.Label_AnguloB.AutoSize = true;
+            this.Label_AnguloB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_AnguloB.Location = new System.Drawing.Point(121, 299);
+            this.Label_AnguloB.Name = "Label_AnguloB";
+            this.Label_AnguloB.Size = new System.Drawing.Size(21, 16);
+            this.Label_AnguloB.TabIndex = 21;
+            this.Label_AnguloB.Text = "0°";
+            // 
+            // Label_AnguloA
+            // 
+            this.Label_AnguloA.AutoSize = true;
+            this.Label_AnguloA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_AnguloA.Location = new System.Drawing.Point(121, 268);
+            this.Label_AnguloA.Name = "Label_AnguloA";
+            this.Label_AnguloA.Size = new System.Drawing.Size(21, 16);
+            this.Label_AnguloA.TabIndex = 20;
+            this.Label_AnguloA.Text = "0°";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label5.Location = new System.Drawing.Point(173, 125);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(29, 16);
+            this.Label5.TabIndex = 19;
+            this.Label5.Text = "90°";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.Location = new System.Drawing.Point(30, 299);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(70, 16);
+            this.Label4.TabIndex = 18;
+            this.Label4.Text = "Angúlo B";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(30, 268);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(70, 16);
+            this.Label3.TabIndex = 17;
+            this.Label3.Text = "Angúlo A";
+            // 
+            // TextBox_Adyacente
+            // 
+            this.TextBox_Adyacente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Adyacente.Location = new System.Drawing.Point(124, 223);
+            this.TextBox_Adyacente.Name = "TextBox_Adyacente";
+            this.TextBox_Adyacente.Size = new System.Drawing.Size(62, 21);
+            this.TextBox_Adyacente.TabIndex = 16;
+            // 
+            // TextBox_Hipotenusa
+            // 
+            this.TextBox_Hipotenusa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_Hipotenusa.Location = new System.Drawing.Point(24, 55);
+            this.TextBox_Hipotenusa.Name = "TextBox_Hipotenusa";
+            this.TextBox_Hipotenusa.Size = new System.Drawing.Size(62, 21);
+            this.TextBox_Hipotenusa.TabIndex = 15;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox1.Image")));
+            this.PictureBox1.Location = new System.Drawing.Point(13, 16);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(266, 218);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 14;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Button_Calular
+            // 
+            this.Button_Calular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Calular.Location = new System.Drawing.Point(33, 347);
+            this.Button_Calular.Name = "Button_Calular";
+            this.Button_Calular.Size = new System.Drawing.Size(75, 23);
+            this.Button_Calular.TabIndex = 13;
+            this.Button_Calular.Text = "Calcular";
+            this.Button_Calular.UseVisualStyleBackColor = true;
+            this.Button_Calular.Click += new System.EventHandler(this.Button_Calular_Click);
+            // 
+            // Button_Limpiar
+            // 
+            this.Button_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Limpiar.Location = new System.Drawing.Point(176, 347);
+            this.Button_Limpiar.Name = "Button_Limpiar";
+            this.Button_Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.Button_Limpiar.TabIndex = 12;
+            this.Button_Limpiar.Text = "Limpiar";
+            this.Button_Limpiar.UseVisualStyleBackColor = true;
+            this.Button_Limpiar.Click += new System.EventHandler(this.Button_Limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(292, 386);
+            this.Controls.Add(this.Label_AnguloB);
+            this.Controls.Add(this.Label_AnguloA);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.TextBox_Adyacente);
+            this.Controls.Add(this.TextBox_Hipotenusa);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Button_Calular);
+            this.Controls.Add(this.Button_Limpiar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular angulo A del triangulo";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label_AnguloB;
+        internal System.Windows.Forms.Label Label_AnguloA;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox TextBox_Adyacente;
+        internal System.Windows.Forms.TextBox TextBox_Hipotenusa;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Button Button_Calular;
+        internal System.Windows.Forms.Button Button_Limpiar;
+    }
+}
+
---
+++ CalcularAngulo A del triangulo/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularAngulo_A_del_triangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Angulos()
+        {
+            //Declaramos la variables de tipo Double
+            double anguloA, anguloB1, anguloB2, adyacente, hipotenusa, coseno;
+            //Condición que valúa el TextBox si está vacío
+            if (TextBox_Hipotenusa.Text == "")
+            {
+                MessageBox.Show("Ingrese la hipotenusa", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //Colocamos el focus en el TextBox
+                TextBox_Hipotenusa.Focus();
+            }
+            else
+            {
+                //Condición que valúa el TextBox si está vacío
+                if (TextBox_Adyacente.Text == "")
+                {
+                    MessageBox.Show("Ingrese la adyacente", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //Colocamos el focus en el TextBox
+                    TextBox_Adyacente.Focus();
+                }
+                else
+                {
+                    /*Métodos que convierte en un tipo double los valores
+                     que se ingresen por los TextBox */
+                    hipotenusa = Convert.ToDouble(TextBox_Hipotenusa.Text);
+                    adyacente = Convert.ToDouble(TextBox_Adyacente.Text);
+                    //División de las variables adyacente/hipotenusa
+                    coseno = adyacente / hipotenusa;
+                    //Formula para sacar el ángulo A sacando el coseno inverso de la variable coseno 
+                    anguloA = Math.Acos(coseno) * 180 / Math.PI;
+                    Label_AnguloA.Text = anguloA.ToString() + "°";
+                    //Método para sacar el ángulo B
+                    anguloB1 = 90 + anguloA;
+                    anguloB2 = 180 - anguloB1;
+                    Label_AnguloB.Text = anguloB2.ToString() + "°";
+                }
+            }
+        }
+
+        private void Button_Calular_Click(object sender, EventArgs e)
+        {
+            Angulos();
+        }
+
+        private void Button_Limpiar_Click(object sender, EventArgs e)
+        {
+            TextBox_Hipotenusa.Text = "";
+            TextBox_Adyacente.Text = "";
+            Label_AnguloA.Text = "0°";
+            Label_AnguloB.Text = "0°";
+            TextBox_Hipotenusa.Focus();
+
+        }
+    }
+}
